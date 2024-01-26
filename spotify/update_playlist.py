b28fb325f5e992a1f72770a983bfb2f9889869b7
Paolo Cremonese <cremonesep25@gmail.com>
2024-01-26 20:57:44 +0100

    added logger, update_playlist and print_playlist_track. Solved login mystery

--- spotify/update_playlist.py
+++ spotify/update_playlist.py
@@ -8,16 +8,23 @@ import spotipy
 import numpy as np
 import telegram_send
 
+from .utils import setup_logger
+
+logger = setup_logger()
+
 # refer all to position
 # make tryouts
 # gitname = 'raspberry'
 # gitpath = os.popen('$HOME/.find_git_path.sh ' + gitname).read()[:-1]
 
+
 class playlist:
 
     def __init__(self, data_folder='data'):
         """
-        variables:
+        variables
+        data_folder: path to 'data' file with usernam, clientID, clientsecret,
+                                            list_id and show_names
         """
         
         with open(f'{data_folder}/data', 'r') as t:
@@ -32,21 +39,40 @@ class playlist:
                        'user-read-playback-position',
                        'user-library-read']
         
-        with open(f'{data_folder}/.cache', 'r') as f:
-            token = f.read()
-        
-        try :
-            self.sp = spotipy.Spotify(auth=token)
-        except:
-            # pass a warning saying you have to login and save token in "data/.cache"
-            # see "https://git.ligo.org/cbc/projects/cbcflow/-/blob/main/src/cbcflow/core/database.py#L31"
-            
-            token = spotipy.util.prompt_for_user_token(self.username, scope=scopes,
-                                               client_id=clientID,
-                                               client_secret=clientsecret,
-                                               redirect_uri='http://localhost:8080/callback')
-            self.sp = spotipy.Spotify(auth=token)
+        # first time it asks to loging, then saves authomatically token in .cache-xxxxxxx
+        token = spotipy.util.prompt_for_user_token(self.username, scope=scopes,
+                                           client_id=clientID,
+                                           client_secret=clientsecret,
+                                           redirect_uri='http://localhost:8080/callback')
+        self.sp = spotipy.Spotify(auth=token)
         
         self.daily_show_names = show_names1.split(",")
         self.long_show_names  = show_names2.split(",")
+    
+        self.playlist = self.sp.playlist(self.list_id, additional_types=('tracks', 'episodes'), market='IT')
+
+
+    def update_playlist(self):
+
+        self.playlist = self.sp.playlist(self.list_id, additional_types=('tracks', 'episodes'), market='IT')
+
+
+    def print_playlist_tracks(self):
+        '''
+        Function to print tracks and episodes of a playlist
+
+        Output:
+        nothing, only print
+        '''
+        tracks = self.playlist['tracks'] 
+        to_print = ''
+        to_print += 'Total number of tracks and episodes: {}\n\n'.format(len(tracks['items']))
+        for ni,item in enumerate(tracks['items']):
+            if 'track' in item:
+                track = item['track']
+            else:
+                track = item
+            to_print += f'{ni+1:02}. {track["name"]}\n'
+        return to_print
+        
          
