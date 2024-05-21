79ab790d36d66722ec78f1985a8f661fea6cc1ae
Paolo Cremonese <cremonesep25@gmail.com>
2024-05-21 14:52:10 +0200

    added files for package

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    version='0.1.0',
+    name="spotify",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        #'',
+        'pandas',
+        'spotipy',
+        'numpy',
+        ]   
+)
---
+++ spotify/__init__.py
@@ -0,0 +1,1 @@
+from spotify.update_playlist import *
