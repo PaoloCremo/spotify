86c734ae5b451620ccb25dc3b21b1a56bc2c0f32
Paolo Cremonese <cremonesep25@gmail.com>
2024-05-21 14:52:46 +0200

    added files for automatic runs

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+conda activate
+python3 spotify/run_up.py
+conda deactivate
---
+++ spotify/run_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/activate
+
+import spotify as sp
+
+if __name__ == "__main__":
+    pl = sp.playlist()
+    pl.main()
